f84c1c75a52cf71a3966576c0eb7af5436ae8142
Anutej Poddaturi <113458527+apoddaturi@users.noreply.github.com>
2023-02-23 14:04:41 -0500

    Feature/ec2 (#7)
    * adds ec2 and removes nat
    
    Adds necessary resources for ec2 and removes nat and related resources
    
    * fmts the tf files
    
    * adding name to security group
    
    * adds packer variables
    
    * makes values as variables
    
    * changes default ami number to base image
    
    * adds timestamp to main ec2 instance

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "my_vpc" {
 
 
 resource "aws_security_group" "sg" {
-  name_prefix = "app-"
+  name_prefix = "app-sg"
   vpc_id      = aws_vpc.my_vpc.id
   ingress {
     from_port   = 22
@@ -35,23 +35,31 @@ resource "aws_security_group" "sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  tags = {
+    Name = "application_security_group"
+  }
 }
 
 resource "aws_key_pair" "deployer" {
   key_name   = "ec2_dev1"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDPRuVxAG7AccT7g8ne5HsDxZwcV8YXPddnJSKQJsLkQG0xEFWfWVGdHmoWGgI5b2WrZAHgpBStteDuD3d/iOr7qhB6efAEMh27Y1wDbhR862nbNmYRhsEqD87S9IeLaIDVUq3sYqrQFp7QtG7cKLSDbZaf8ZHHZwxpc5phxJtBC4tz6KZgPuxQevl/nIh9hWuxoMoOWmoVBGfFzHLoncmMeg1FVYqKKc/sseGhopWuwe3YzzE19pUcNa+xjJDBjcoHW1vYVoKKQN7/GzZvrwEx7qILIC0zF3uuTby0kT7DSjwt7BXBieMdogvSrsfL7mtZI1Ls8Mx8jrkyfKPxmeMr8aZAhJgha+nL6RCXEbNDlamtxZb8rI/Od2wNhi1XTOx/jB+Bvr01cwUkSJsSo6EDL5Bux5RTEWRBn1pQUfP/54WzaOIlDKM245iglzzUxB0g0MiU+31xXGskcvh/GGctULKd5XpOZjKM4jGbA4WijfZ/owOIj9bfhfADuAqYuTk= podda@Anutej-XPS-15"
+  public_key = var.ssh_key
 }
 
 resource "aws_instance" "example" {
-  ami                    = "ami-059d46e63623bc0df"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.sg.id]
-  subnet_id              = element(aws_subnet.public_subnet.*.id, 0)
-  key_name               = aws_key_pair.deployer.key_name
+  ami                     = var.ami_val
+  instance_type           = "t2.micro"
+  vpc_security_group_ids  = [aws_security_group.sg.id]
+  subnet_id               = element(aws_subnet.public_subnet.*.id, 0)
+  key_name                = aws_key_pair.deployer.key_name
+  disable_api_termination = true
+
   root_block_device {
     volume_size           = 50
     volume_type           = "gp2"
     delete_on_termination = true
   }
-  disable_api_termination = true
+  tags = {
+    "Name" = "Ec2_Terraform-${timestamp()}"
+  }
+
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,7 +1,7 @@
-region                 = "us-east-2"
+region                 = "us-east-1"
 profile                = "dev"
 cidr                   = "10.0.0.0/16"
 igw_cidr               = "0.0.0.0/0"
 public_cidrs           = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 private_cidrs          = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
-aws_availability_zones = ["us-east-2a", "us-east-2b", "us-east-2c"]
+aws_availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
