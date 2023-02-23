d7af978d8d4027a1b3d8573c7fbdf1487306f57b
Anutej Poddaturi <poddaturi.a@northeastern.edu>
2023-02-23 13:03:32 -0500

    adds packer variables

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -9,24 +9,24 @@ resource "aws_vpc" "my_vpc" {
 
 
 resource "aws_security_group" "sg" {
-  name_prefix = "app-"
+  name_prefix = "app-sg"
   vpc_id      = aws_vpc.my_vpc.id
   ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "ssh"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "http"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 443
     to_port     = 443
-    protocol    = "https"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
@@ -42,19 +42,22 @@ resource "aws_security_group" "sg" {
 
 resource "aws_key_pair" "deployer" {
   key_name   = "ec2_dev1"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDPRuVxAG7AccT7g8ne5HsDxZwcV8YXPddnJSKQJsLkQG0xEFWfWVGdHmoWGgI5b2WrZAHgpBStteDuD3d/iOr7qhB6efAEMh27Y1wDbhR862nbNmYRhsEqD87S9IeLaIDVUq3sYqrQFp7QtG7cKLSDbZaf8ZHHZwxpc5phxJtBC4tz6KZgPuxQevl/nIh9hWuxoMoOWmoVBGfFzHLoncmMeg1FVYqKKc/sseGhopWuwe3YzzE19pUcNa+xjJDBjcoHW1vYVoKKQN7/GzZvrwEx7qILIC0zF3uuTby0kT7DSjwt7BXBieMdogvSrsfL7mtZI1Ls8Mx8jrkyfKPxmeMr8aZAhJgha+nL6RCXEbNDlamtxZb8rI/Od2wNhi1XTOx/jB+Bvr01cwUkSJsSo6EDL5Bux5RTEWRBn1pQUfP/54WzaOIlDKM245iglzzUxB0g0MiU+31xXGskcvh/GGctULKd5XpOZjKM4jGbA4WijfZ/owOIj9bfhfADuAqYuTk= podda@Anutej-XPS-15"
+  public_key = var.ssh_key
 }
 
 resource "aws_instance" "example" {
-  ami                    = "ami-059d46e63623bc0df"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.sg.id]
-  subnet_id              = element(aws_subnet.public_subnet.*.id, 0)
-  key_name               = aws_key_pair.deployer.key_name
+  ami                     = "ami-071b0fc939ab10537"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids  = [aws_security_group.sg.id]
+  subnet_id               = element(aws_subnet.public_subnet.*.id, 0)
+  key_name                = aws_key_pair.deployer.key_name
+  disable_api_termination = false
   root_block_device {
-    volume_size           = 50
+    volume_size           = 8
     volume_type           = "gp2"
     delete_on_termination = true
   }
-  disable_api_termination = true
+  tags = {
+    "Name" = "Ec2_Terraform-{{timestamp}}"
+  }
 }
--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -19,3 +19,7 @@ variable "aws_availability_zones" {
   type = list(string)
 }
 
+variable "ssh_key" {
+  type    = string
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDPRuVxAG7AccT7g8ne5HsDxZwcV8YXPddnJSKQJsLkQG0xEFWfWVGdHmoWGgI5b2WrZAHgpBStteDuD3d/iOr7qhB6efAEMh27Y1wDbhR862nbNmYRhsEqD87S9IeLaIDVUq3sYqrQFp7QtG7cKLSDbZaf8ZHHZwxpc5phxJtBC4tz6KZgPuxQevl/nIh9hWuxoMoOWmoVBGfFzHLoncmMeg1FVYqKKc/sseGhopWuwe3YzzE19pUcNa+xjJDBjcoHW1vYVoKKQN7/GzZvrwEx7qILIC0zF3uuTby0kT7DSjwt7BXBieMdogvSrsfL7mtZI1Ls8Mx8jrkyfKPxmeMr8aZAhJgha+nL6RCXEbNDlamtxZb8rI/Od2wNhi1XTOx/jB+Bvr01cwUkSJsSo6EDL5Bux5RTEWRBn1pQUfP/54WzaOIlDKM245iglzzUxB0g0MiU+31xXGskcvh/GGctULKd5XpOZjKM4jGbA4WijfZ/owOIj9bfhfADuAqYuTk= podda@Anutej-XPS-15"
+}
